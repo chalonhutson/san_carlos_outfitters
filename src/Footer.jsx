786ce363c9caf1eb4ca70064d4c6563daa012c23
Chalon Hutson <chalonhutson@gmail.com>
2024-02-27 08:41:24 -0700

    Updated main page.

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <div className="footerContainer">
+            <div className="footerInfo">
+                <h3>SAN CARLOS OUTFITTERS</h3>
+                <h3>-</h3>
+                <h3>Call (480) 204-3131</h3>
+            </div>
+            <div className="footerInfo">
+                <p>Site Designed By Chalon Hutson</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import Footer from "../Footer"
+
+import HuntingImg from "../images/hunting_behind_photo.jpg"
+import DeerImg from "../images/deer_photo.jpg"
+import CoupleImg from "../images/couple_walking_photo.jpg"
+
+import Arrow from "../icons/arrow_circle_right.png"
 
 export default function Home() {
     return (
@@ -6,17 +12,22 @@ export default function Home() {
             <div className="landingTopContainer">
                 <span className="landingHeaderContainer">
                     <h2 className="landingHeader">
-                        HOME
+                        SAN CARLOS OUTFITTERS
                     </h2>
-                    <h2 className="landingHeader">
-                        ABOUT
-                    </h2>
-                    <h2 className="landingHeader">
-                        PRICING
-                    </h2>
-                    <h2 className="landingHeader">
-                        CONTACT
-                    </h2>
+                    <span className="landingLinksContainer">
+                        <h2 className="landingLink">
+                            HOME
+                        </h2>
+                        <h2 className="landingLink">
+                            ABOUT
+                        </h2>
+                        <h2 className="landingLink">
+                            PRICING
+                        </h2>
+                        <h2 className="landingLink">
+                            CONTACT
+                        </h2>
+                    </span>
                 </span>
                 <span className="landingInformation">
                     <div className="landingInformationContainer">
@@ -27,17 +38,48 @@ export default function Home() {
                         <p>
                             Our guided expeditions on sovereign Native lands offer a unique opportunity to connect with the wilderness as never before. Led by expert guides from local communities, you'll experience hunting in its most authentic form, respecting the land and its traditions. Join us for an unforgettable adventure where heritage and the great outdoors come together for a truly remarkable experience.
                         </p>
-                        <span>
-                            <button>
+                        <span className="landingBtnContainer">
+                            <button className="scheduleNowBtn">
                                 SCHEDULE NOW
                             </button>
-                            <button>
+                            <span className="learnMoreBtn">
                                 LEARN MORE
-                            </button>
+                                <img src={Arrow} />
+                            </span>
                         </span>
                     </div>
                 </span>
             </div>
+            <div className="landingMiddleContainer">
+                <div className="infoContainer">
+                    <img src={HuntingImg} />
+                    <h3>What We Provide</h3>
+                    <p>
+                        Don’t have your own equipment? No worries! We rent at a premium rifles, packs, gear, and more. You can bring your own equipment, or allow us to cover all your hunting needs.
+                    </p>
+                </div>
+                <div className="infoContainer">
+                    <img src={DeerImg} />
+                    <h3>Where To Go</h3>
+                    <p>
+                        We know where the best hunting spots are on the reservation. Want to have the best day possible, allow our guides to take you there!
+                    </p>
+                </div>
+                <div className="infoContainer">
+                    <img src={CoupleImg} />
+                    <h3>An Experience Like No Other</h3>
+                    <p>
+                        Friends and family are all invited. We will accommodate groups and ensure everyone can have a fun day out hunting.
+                    </p>
+                </div>
+            </div>
+            <div className="landingBottomCTA">
+                <h1>Questions? Please reach out!</h1>
+                <button className="scheduleNowBtn">
+                    CONTACT US
+                </button>
+            </div>
+            <Footer />
         </div>
     )
 }
